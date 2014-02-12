f0c78bc51fd17edfdf6366db6eb2d27cf9e47324
sathvik1709 <sathvikshivaprakash519@gmail.com>
2014-02-12 02:02:05 -0600

    login screen ready

--- proj/gen/com/sathvik/the_brain/everything/R.java
+++ proj/gen/com/sathvik/the_brain/everything/R.java
@@ -27,8 +27,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
+        public static final int action_settings=0x7f090005;
+        public static final int editText1=0x7f090002;
+        public static final int editText2=0x7f090004;
         public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090001;
+        public static final int textView3=0x7f090003;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -39,7 +43,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int enter_password=0x7f060006;
+        public static final int enter_username=0x7f060005;
         public static final int hello_world=0x7f060002;
+        public static final int pass_word=0x7f060007;
+        public static final int user_name=0x7f060004;
         public static final int welcome_note=0x7f060003;
     }
     public static final class style {
